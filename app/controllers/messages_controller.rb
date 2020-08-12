2cff5b1e4da8c7f9bec03fe9e5631adf9924603f
shimokariya yuki <shimokari0321@gmail.com>
2020-08-12 20:41:26 +0900

    チャットルームの削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,10 +15,10 @@ class MessagesController < ApplicationController
     end
   end
 
-
   private
 
   def message_params
     params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
+
